83ea44130c449da549c8dfad6a855c13baeef0fd
Qiao, Joe <qiao.y@pg.com>
2022-02-18 18:28:01 +0800

    cb

---
+++ kafka-java/src/main/java/org/joe/learning/kafka/first/FirstProducerCb.java
@@ -0,0 +1,26 @@
+package org.joe.learning.kafka.first;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class FirstProducer {
+    public static void main(String[] args) {
+        String bootstrapServer = "127.0.0.1:9092";
+
+        System.out.println("hello world");
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+        ProducerRecord<String, String> record = new ProducerRecord<>("joe-first", "hello from intellij");
+
+        producer.send(record);
+        producer.close();
+    }
+}
